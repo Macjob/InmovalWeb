797c3e3e48aecd6dccc939a0033b840205f22479
Migue <macjob@gmail.com>
2023-07-13 01:14:57 -0400

    prueba

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,9 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/inmoval/',
+  base: '/InmovalWeb/',
   plugins: [vue()],
+  alias: {
+    '@': path.resolve(__dirname, './src'),
+  },
 })
